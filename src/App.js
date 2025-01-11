4148efd07e82760892fef4151da8af9af156dcc6
Felipe <filipespania777@gmail.com>
2025-01-11 21:59:48 +0100

    Added a NotFound page for error 404

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BookProvider } from './BookContext';
 import Books from './components/Books';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import BookDetails from './components/BookDetails';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Books />} />
           <Route path="/books" element={<Navigate to="/" />} />
           <Route path='/books/:bookId' element={<BookDetails />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </Router>
     </BookProvider>
